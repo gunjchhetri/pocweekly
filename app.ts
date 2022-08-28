21d09c1a1f3db18f372f5f961566902b2d8d0d0b
Gunjan Chhetri <gunjan.chhetri88@gmail.com>
2022-08-27 23:18:02 -0700

    commit

---
+++ app.ts
@@ -0,0 +1,6 @@
+import { createExpressServer } from 'routing-controllers';
+import path from 'path';
+
+createExpressServer({
+  controllers: [path.join(__dirname + '/controllers/*.js')],
+}).listen(3000, ()=>console.log('server working'));
